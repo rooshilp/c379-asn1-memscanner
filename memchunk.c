00bb48daeaa2ddf312d79a11232f1269c18c8cff
rooshilp <rooshilp@gmail.com>
2014-01-30 11:48:18 -0700

    added to get_mem_layout function

--- memchunk.c
+++ memchunk.c
@@ -22,10 +22,63 @@ struct memchunk {
 int get_mem_layout (struct memchunk *chunk_list, int size) {
         int pageSize = getpagesize();
         int chunkCount = 0;
+	int end = 0xffffffff
+	int listIndex = 0
 	void *memLocation;
 	memLocation = (void*) 0x00000000
 	
+	while (memLocation < end) {
+		int access;
+		int lastAccess;
+		int chunkSize;
+		access = check_access(*memLocation)
+		if (access == -1) {
+			if (lastAccess == access) {
+				continue;
+			}
+			else {
+				chunkCount++
+				
+		if (access == 0) {
+			if (lastAccess == access) {
+				chunkSize++;
+				chunk_list[listIndex].length += pageSize;
+			}
+			else {
+				if (lastAccess == 1) {
+					chunkCount++;
+				}
+				chunkSize = 1
+				listIndex++;
+				lastAccess = 0;
+				chunk_list[listIndex].*start = *memLocation;
+				chunk_list[listIndex].length = 0;
+				chunk_list[listIndex].length += pageSize;
+				chunk_list[listIndex].RW = 0;
+			}
+		}
+
+		if (access == 1) {
+			if (lastAccess == access) {
+				chunkSize++;
+				chunk_list[listIndex].length += pageSize;
+			}
+			else {
+				if (lastAccess == 0) {
+					chunkCount++;
+				}
+				chunkSize = 1
+				listIndex++;
+				lastAccess = 1;
+				chunk_list[listIndex].*start = *memLocation;
+				chunk_list[listIndex].length = 0;
+				chunk_list[listIndex].length += pageSize;
+				chunk_list[listIndex].RW = 1;
+			}
+		}
 }
+
+
 
 int check_access (void *memLocation) {
         signal(SIGSEGV, signal_handler);
@@ -33,8 +86,10 @@ int check_access (void *memLocation) {
 	char var;
         int sig = sigsetjmp(env, 1);        
         if (sig == 0) {
+		printf("Reading address.");
                 char var = (char*)memLocation;
 		returnValue = 0;
+		printf("Writing to address.");
 		(char*)memLocation = (char*)memLocation;
 		returnValue = 1;
         }
