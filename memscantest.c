9e9fef475204dcd0829734eeaea8827cae58d08d
rooshilp <rooshilp@gmail.com>
2014-01-31 17:33:43 -0700

    Program slightly updated and bugfixed and asn1writeup.txt completed.

--- memscantest.c
+++ memscantest.c
@@ -20,7 +20,7 @@ int main()
 	struct memchunk chunk_list[20];
 	size = 20;
 	chunkCount = get_mem_layout(&chunk_list, size);
-	printf("%d\n", chunkCount);
+	printf("Number of chunks: %d\n", chunkCount);
 	int i;
 	for (i = 0;i < chunkCount; i++) {
 		printf("%p - %d - %d\n", chunk_list[i].start, chunk_list[i].length, chunk_list[i].RW);
